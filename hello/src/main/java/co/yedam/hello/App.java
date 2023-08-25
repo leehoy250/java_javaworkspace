48a0485419b8d391d79fb39691609c5835b0e819
admin <admin@YD5-18>
2023-08-25 17:55:12 +0900

    0825 수업

---
+++ hello/src/main/java/co/yedam/hello/App.java
@@ -0,0 +1,12 @@
+package co.yedam.hello;
+
+import co.yedam.hello.menu.ProductManager;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ProductManager menu = new ProductManager();
+    	menu.run();
+    }
+}
---
+++ hello/src/main/java/co/yedam/hello/product/service/ProductService.java
@@ -0,0 +1,11 @@
+package co.yedam.hello.product.service;
+
+import java.util.List;
+
+public interface ProductService {
+	List<ProductVO> productSelectList();   // R 전체조회
+	ProductVO productSelect(ProductVO vo); // R 하나의 제품 조회
+	int productInsert(ProductVO vo); // C 제품 등록 / productInsert = product: 테이블명, Insert: 쿼리명 
+	int productDelete(ProductVO vo); // D 제품 삭제
+	int productUpdate(ProductVO vo); // U 제품 변경
+}
